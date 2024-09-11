638c8091c784891b8dbf66e8706c08c92d6a8057
Jack Bodworth <jack.bodsworth@unimelb.edu.au>
2024-09-11 10:06:31 +1000

    Initial Commit

---
+++ WeatherDemo/WeatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemo
+//
+//  Created by Jack Bodsworth on 11/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
